0859d1259f972f6546b6c92a704acb44bc670b9a
Etzel <etzelmm@gmail.com>
2026-04-03 10:37:19 -0500

    initial commit

---
+++ Modulo_5/demo/src/main/java/com/devsenior/students/eamolinam/demo_springboot_web/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.students.eamolinam.demo_springboot_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ Modulo_5/demo/src/test/java/com/devsenior/students/eamolinam/demo_springboot_web/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.students.eamolinam.demo_springboot_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
